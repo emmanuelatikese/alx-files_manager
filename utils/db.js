99d4db526997b87ecc9b055beff0bf4ad12ac13d
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-08-07 13:26:23 +0100

    adding

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import { Mongoclient } from 'mongodb';
+import { MongoClient } from 'mongodb';
 
 dotenv.config();
 
@@ -7,13 +7,38 @@ class DBClient {
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'file_manager';
+    const dbName = process.env.DB_DATABASE || 'file_manager';
+    const url = `mongodb://${host}:${port}`;
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
     this.isConnected = false;
-    this.client = new Mongoclient(`mongodb://${host}:${port}`);
-    this.client.connect(() => {
-      this.isConnected = true;
-    });
-    this.db = this.client.db(database);
+    this.client.connect()
+      .then(() => {
+        this.isConnected = true;
+        this.db = this.client.db(dbName);
+      })
+      .catch((err) => console.error(err));
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async nbUsers() {
+    try {
+      const count = await this.db.collection('users').countDocuments();
+      return count;
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+
+  async nbFiles() {
+    try {
+      const count = await this.db.collection('files').countDocuments();
+      return count;
+    } catch (err) {
+      throw new Error(err);
+    }
   }
 }
 const dbClient = new DBClient();
